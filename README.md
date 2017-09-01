bb40fad0ca5e56e9fb49b705eedfdda668ff8537
zhengmeiyu <zhengmeiyu@baidu.com>
2017-09-01 18:02:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# generator-web-app
+
+## 功能
+- 代码格式检查
+- git ci push hook
+- 编辑器代码风格
+- 启动本地服务，默认监听端口8888
+- css文件能自动补全css3属性的前缀
+- 使用命令npm run dev启动服务，修改保存文件的时候浏览器会自动刷新
+- 使用npm run build打包文件，js和css分开打包，并且默认会压缩文件
---
+++ generators/app/temp.js
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2017 baidu.com
+ * index.js
+ *
+ * changelog
+ * 2017-08-24[10:26:42]:revised
+ *
+ * @author yinyong02@baidu.com
+ * @version 1.0.0
+ * @since 1.0.0
+ * @file
+ */
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const moment = require('moment');
+const _ = require('lodash');
+
+module.exports = class extends Generator {
+    prompting() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the amazing ' + chalk.red('generator-superfe-hn') + ' generator!'
+        ));
+
+        var prompts = [{
+            type: 'input',
+            name: 'name',
+            message: 'Your project name?',
+            required: true,
+            default: this.appname
+        }, {
+            type: 'input',
+            name: 'namespace',
+            message: 'Your project\'s namespace on http://icode.baidu.com?',
+            required: true,
+            default: 'feedhn'
+        }, {
+            type: 'input',
+            name: 'userName',
+            message: 'Your name?',
+            required: true,
+            default: this.user.git.name()
+        }, {
+            type: 'input',
+            name: 'userEmail',
+            message: 'Your email?',
+            required: true,
+            default: this.user.git.email()
+        }];
+
+        return this.prompt(prompts).then(function (props) {
+            const m = moment();
+            props.now = m.format('YYYY-MM-DD') + '[' + m.format('HH:mm:ss') + ']'; // 2016-11-14[13:52:13]
+            props.today = m.format('YYYY-MM-DD'); // 2016-11-14
+            props.componentName = _.upperFirst(_.camelCase(props.name));
+            this.props = props;
+        }.bind(this));
+    }
+
+    writing() {
+        const metaFiles =
+            'editorconfig,babelrc,buckconfig,eslintrc,flowconfig,gitignore,npmignore,npmrc,watchmanconfig'.split(',');
+        const normalFiles =
+            'README.md,package.json,CHANGELOG.md,BCLOUD,src'.split(',');
+        const copyFiles = 'gulpfile.babel.js,ios,tools,bundlePackages.sh,index.html'.split(',');
+
+        metaFiles.forEach(meta => {
+            this.fs.copy(
+                this.templatePath(meta),
+                this.destinationPath(`.${meta}`),
+                this.props
+            );
+        });
+        normalFiles.forEach(meta => {
+            this.fs.copyTpl(
+                this.templatePath(meta),
+                this.destinationPath(meta),
+                this.props
+            );
+        });
+        copyFiles.forEach(meta => {
+            this.fs.copy(
+                this.templatePath(meta),
+                this.destinationPath(meta),
+                this.props
+            );
+        });
+    }
+
+    install() {
+        this.installDependencies({
+            npm: true,
+            bower: false,
+            yarn: false,
+            callback: () => {
+                this.log('HN开发指南: http://wiki.baidu.com/pages/viewpage.action?pageId=372648654');
+            }
+        });
+    }
+};
